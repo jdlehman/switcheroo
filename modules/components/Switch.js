14aafea57467611cdf2a6b55bb1c7e03418c3ff5
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-04-03 20:08:24 -0400

    Add optional onHide/onShow hooks to Switch component
    - Allows functions to be specified to run when Switch is shown/hidden

--- modules/components/Switch.js
+++ modules/components/Switch.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 
 export default class Switch extends Component {
+  componentDidMount() {
+    if(typeof this.props.onShow === 'function') {
+      this.props.onShow();
+    }
+  }
+
+  componentWillUnmount() {
+    if(typeof this.props.onHide === 'function') {
+      this.props.onHide();
+    }
+  }
+
   render() {
     return React.createElement(this.props.handler, this.props.handlerProps);
   }
@@ -11,5 +23,7 @@ Switch.displayName = 'Switch';
 Switch.propTypes = {
   path: React.PropTypes.string.isRequired,
   handler: React.PropTypes.func.isRequired,
-  handlerProps: React.PropTypes.object
+  handlerProps: React.PropTypes.object,
+  onShow: React.PropTypes.func,
+  onHide: React.PropTypes.func
 };
