ceb141896beb84b1cbf09abf0515e9c3971a7c43
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-07-05 20:21:45 -0400

    Add basic example

---
+++ basic/components/View2.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class View2 extends Component {
+  render() {
+    return (
+      <div>
+        This is view 2
+      </div>
+    );
+  }
+}
---
+++ basic/webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules'],
+    fallback: __dirname
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
+    ]
+  },
+  externals: [
+    {
+      'window': 'window'
+    }
+  ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
