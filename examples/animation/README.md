00892feee41fbe45b3ba1e3eecd0856f5d9310c6
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-10-20 17:02:39 -0400

    Add animations example
    - Shows how to use wrapper property of switcher
    - CSS animations and JS animations

---
+++ examples/animation/README.md
@@ -0,0 +1,7 @@
+# Basic Example
+
+## Running locally
+
+- `npm install`
+- `npm start`
+- navigate to `http://localhost:8000`
---
+++ examples/animation/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/App';
+
+require('app.css');
+
+ReactDOM.render(<App />, document.getElementById('root'));
