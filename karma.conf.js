fe83415285f21d2b1cef61c2033c1bb759fbd7f7
Michael Peteuil <michael.peteuil@gmail.com>
2015-10-30 15:51:11 -0400

    Remove unnecessary karma-sinon plugin
    Doesn't provide any real obvious benefits in this case. This change was
    also made in jdlehman/jsrouter (https://github.com/jdlehman/jsrouter/commit/4824580f73cc54fe76c9f1ea481c8120aaa7d57a).

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
@@ -126,7 +126,6 @@ module.exports = function(config) {
       'karma-chai',
       'karma-webpack',
       'karma-sourcemap-loader',
-      'karma-sinon',
       'karma-phantomjs-launcher',
       'karma-coverage'
     ],
