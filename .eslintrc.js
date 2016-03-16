c591230ff0488b6e82f24a383719a9f0d2c47fea
Jonathan Lehman <jonathan.lehman91@gmail.com>
2016-03-16 12:33:54 -0400

    Update dependencies and fix linting failure on travis

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'extends': [
+    'customink',
+    'customink/browser',
+    'customink/mocha',
+    'customink/react'
+  ],
+
+  'globals': {
+    'require': true,
+    'process': true,
+    'global': true,
+    'module': true,
+  }
+};
