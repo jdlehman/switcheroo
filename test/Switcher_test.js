79657486da33b0487acbc8404e4188830a97e00c
Jonathan Lehman <jonathan.lehman91@gmail.com>
2017-04-20 10:43:29 -0400

    Use prop-types package to prepare for PropTypes deprecation

--- test/Switcher_test.js
+++ test/Switcher_test.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import {assert} from 'chai';
 import sinon from 'sinon';
