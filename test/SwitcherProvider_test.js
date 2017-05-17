54e823506ad2cab75dc0e5e03357da5d6fbdbec4
Jonathan Lehman <jonathan.lehman91@gmail.com>
2017-05-17 08:59:28 -0400

    Add test for SwitcherProvider with multiple children

--- test/SwitcherProvider_test.js
+++ test/SwitcherProvider_test.js
@@ -4,8 +4,8 @@ import sinon from 'sinon';
 import Switcher, { SwitcherProvider } from '../src';
 import * as helpers from '../src/helpers';
 
-describe('SwitcherProvider', function() {
-  it('renders correctly', function() {
+describe('SwitcherProvider', () => {
+  it('renders correctly', () => {
     sinon.stub(helpers, 'currentPath').returns('/');
     const component = renderComponent();
     expect(component.text()).toEqual('HomeHome');
@@ -16,11 +16,36 @@ describe('SwitcherProvider', function() {
     expect(component.text()).toEqual('Second');
     helpers.currentPath.restore();
   });
+
+  it('wraps a span around multiple children', () => {
+    sinon.stub(helpers, 'currentPath').returns('/');
+    const component = renderComponentWithManyChildren();
+    expect(component.find('.switcher-provider').length).toEqual(1);
+    expect(component.text()).toEqual('Another ChildHomeHome');
+    helpers.currentPath.restore();
+  });
 });
 
 function renderComponent() {
   return mount(
     <SwitcherProvider>
+      <div>
+        <Switcher>
+          <div path="/">Home</div>
+        </Switcher>
+        <Switcher>
+          <div path={['/', '/other']}>Home</div>
+          <div path="/second">Second</div>
+        </Switcher>
+      </div>
+    </SwitcherProvider>
+  );
+}
+
+function renderComponentWithManyChildren() {
+  return mount(
+    <SwitcherProvider>
+      <div>Another Child</div>
       <div>
         <Switcher>
           <div path="/">Home</div>
