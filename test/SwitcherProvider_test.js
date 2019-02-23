f34ad190e95db631e047bcaadb9eb0ce96153f34
Matt Thompson <whatknight@gmail.com>
2019-02-23 15:30:38 -0500

    fix act warning by using mocked timers

--- test/SwitcherProvider_test.js
+++ test/SwitcherProvider_test.js
@@ -1,12 +1,5 @@
-import React, { useEffect, useReducer } from 'react';
-import { act } from 'react-dom/test-utils';
-import PropTypes from 'prop-types';
-import {
-  render,
-  fireEvent,
-  waitForDomChange,
-  wait
-} from 'react-testing-library';
+import React from 'react';
+import { render, fireEvent, act } from 'react-testing-library';
 import Switcher, { SwitcherProvider } from '../src';
 import * as helpers from '../src/helpers';
 import { ContextReporter } from './utils';
@@ -32,90 +25,42 @@ describe('SwitcherProvider', () => {
     expect(component.container).toHaveTextContent('Another ChildHomeHome');
   });
 
+  jest.useFakeTimers();
   test('removes event listeners when a child Switcher is unmounted', async () => {
     helpers.generateGuid = jest
       .fn()
       .mockReturnValueOnce('bottom')
       .mockReturnValueOnce('top');
     helpers.currentPath = jest.fn(() => ({ path: '/', params: {} }));
-
     const component = renderNested();
-
-    await wait(() => {
-      const loadListeners = Array.from(
-        component.container.querySelectorAll('.loadListeners em')
-      ).map(el => el.textContent);
-      expect(loadListeners).toEqual(['bottom', 'top']);
-
-      const hashChangeListeners = Array.from(
-        component.container.querySelectorAll('.hashChangeListeners em')
-      ).map(el => el.textContent);
-      expect(hashChangeListeners).toEqual(['bottom', 'top']);
+    act(() => {
+      jest.runAllTimers();
     });
+    const getListeners = type => {
+      return Array.from(
+        component.container.querySelectorAll(`.${type}Listeners em`)
+      ).map(el => el.textContent);
+    };
+
+    expect(getListeners('load')).toEqual(['bottom', 'top']);
+    expect(getListeners('hashChange')).toEqual(['bottom', 'top']);
 
     helpers.currentPath = jest.fn(() => ({ path: '/hello', params: {} }));
     // trigger hash change manually
     fireEvent(window, new HashChangeEvent('hashchange'));
-    await wait(() => {
-      const loadListeners = Array.from(
-        component.container.querySelectorAll('.loadListeners em')
-      ).map(el => el.textContent);
-      expect(loadListeners).toEqual(['top']);
+    act(jest.runAllTimers);
 
-      const hashChangeListeners = Array.from(
-        component.container.querySelectorAll('.hashChangeListeners em')
-      ).map(el => el.textContent);
-      expect(hashChangeListeners).toEqual(['top']);
-    });
+    expect(getListeners('load')).toEqual(['top']);
+    expect(getListeners('hashChange')).toEqual(['top']);
   });
 });
 
 const SwitchedTo = ({ children }) => <div>{children}</div>;
 
-const ContextReporter = (
-  _,
-  { switcherProvider: { loadListeners, hashChangeListeners } }
-) => {
-  const [, forceUpdate] = useReducer(x => x + 1, 0);
-  //refresh bc old context SUX
-  useEffect(() => {
-    setTimeout(forceUpdate);
-  }, []);
-
-  useEffect(() => {
-    const doIt = () => {
-      // because we can't rely on order of rendering here, we'll have to wait
-      // oh yeah and old context SUX
-      setTimeout(forceUpdate);
-    };
-    window.addEventListener('hashchange', doIt);
-    return () => {
-      window.removeEventListener('hashchange', doIt);
-    };
-  }, []);
-  return (
-    <article>
-      <p className="loadListeners">
-        {loadListeners.map(({ id }) => (
-          <em key={id}>{id}</em>
-        ))}
-      </p>
-      <p className="hashChangeListeners">
-        {hashChangeListeners.map(({ id }) => (
-          <em key={id}>{id}</em>
-        ))}
-      </p>
-    </article>
-  );
-};
-
-ContextReporter.contextTypes = {
-  switcherProvider: PropTypes.object
-};
-
 function renderNested() {
   return render(
     <SwitcherProvider>
+      <ContextReporter />
       <div>
         <Switcher>
           <Switcher path="/">
@@ -124,7 +69,6 @@ function renderNested() {
           </Switcher>
           <SwitchedTo path="/hello">Hello</SwitchedTo>
         </Switcher>
-        <ContextReporter />
       </div>
     </SwitcherProvider>
   );
