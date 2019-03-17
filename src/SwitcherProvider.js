3f51336bd164d331a3dd31547acbda9505b7ec21
Matt Thompson <whatknight@gmail.com>
2019-03-17 10:59:30 -0400

    refactor context so its has less hacks

--- src/SwitcherProvider.js
+++ src/SwitcherProvider.js
@@ -1,4 +1,10 @@
-import React, { Children, useRef, useEffect } from 'react';
+import React, {
+  Children,
+  useRef,
+  useEffect,
+  useCallback,
+  useMemo
+} from 'react';
 import PropTypes from 'prop-types';
 import SwitcherContext from './context';
 
@@ -11,11 +17,11 @@ export default function SwitcherProvider(props) {
 
   useEffect(() => {
     const handleLoadListeners = e =>
-      switcherProvider.current.loadListeners.forEach(({ fn }) => fn(e));
+      switcherProvider.current.loadListeners.forEach(fn => fn(e));
     const handlePopStateListeners = e =>
-      switcherProvider.current.popStateListeners.forEach(({ fn }) => fn(e));
+      switcherProvider.current.popStateListeners.forEach(fn => fn(e));
     const handleHashChangeListeners = e =>
-      switcherProvider.current.hashChangeListeners.forEach(({ fn }) => fn(e));
+      switcherProvider.current.hashChangeListeners.forEach(fn => fn(e));
     window.addEventListener('load', handleLoadListeners);
     window.addEventListener('popstate', handlePopStateListeners);
     window.addEventListener('hashchange', handleHashChangeListeners);
@@ -26,8 +32,43 @@ export default function SwitcherProvider(props) {
     };
   }, []);
 
+  const getListenerList = useCallback(type => {
+    switch (type) {
+      case 'load':
+        return switcherProvider.current.loadListeners;
+      case 'popstate':
+        return switcherProvider.current.popStateListeners;
+      case 'hashchange':
+        return switcherProvider.current.hashChangeListeners;
+      default:
+        throw new Error('womp womp');
+    }
+  }, []);
+
+  const addListener = useCallback(
+    (type, fn) => {
+      getListenerList(type).push(fn);
+    },
+    [getListenerList]
+  );
+
+  const removeListener = useCallback(
+    (type, fn) => {
+      const listeners = getListenerList(type);
+      listeners.splice(listeners.indexOf(fn), 1);
+    },
+    [getListenerList]
+  );
+
+  const providedMethods = useMemo(() => {
+    return {
+      addListener,
+      removeListener
+    };
+  }, [addListener, removeListener]);
+
   return (
-    <SwitcherContext.Provider value={switcherProvider.current}>
+    <SwitcherContext.Provider value={providedMethods}>
       {(() => {
         if (Children.count(props.children) > 1) {
           return <span className="switcher-provider">{props.children}</span>;
--- test/SwitcherProvider_test.js
+++ test/SwitcherProvider_test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, fireEvent, act } from 'react-testing-library';
 import Switcher, { SwitcherProvider } from '../src';
 import * as helpers from '../src/helpers';
-import { ContextReporter } from './utils';
 
 describe('SwitcherProvider', () => {
   afterEach(() => jest.restoreAllMocks());
@@ -25,45 +24,35 @@ describe('SwitcherProvider', () => {
     expect(component.container).toHaveTextContent('Another ChildHomeHome');
   });
 
-  jest.useFakeTimers();
   test('removes event listeners when a child Switcher is unmounted', async () => {
-    helpers.generateGuid = jest
-      .fn()
-      .mockReturnValueOnce('bottom')
-      .mockReturnValueOnce('top');
     helpers.currentPath = jest.fn(() => ({ path: '/', params: {} }));
+    const [top, bottom] = [jest.fn(), jest.fn()];
+    renderNested({ top, bottom });
 
-    const component = renderNested();
-    act(jest.runAllTimers);
-
-    const getListeners = type => {
-      return Array.from(
-        component.container.querySelectorAll(`.${type}Listeners em`)
-      ).map(el => el.textContent);
-    };
-
-    expect(getListeners('load')).toEqual(['bottom', 'top']);
-    expect(getListeners('hashChange')).toEqual(['bottom', 'top']);
+    fireEvent(window, new Event('load'));
+    expect(top).toHaveBeenCalledTimes(1);
+    expect(bottom).toHaveBeenCalledTimes(1);
 
     helpers.currentPath = jest.fn(() => ({ path: '/hello', params: {} }));
     // trigger hash change manually
     fireEvent(window, new HashChangeEvent('hashchange'));
-    act(jest.runAllTimers);
 
-    expect(getListeners('load')).toEqual(['top']);
-    expect(getListeners('hashChange')).toEqual(['top']);
+    expect(top).toHaveBeenCalledTimes(2);
+    expect(bottom).toHaveBeenCalledTimes(2);
+    fireEvent(window, new HashChangeEvent('hashchange'));
+    expect(top).toHaveBeenCalledTimes(3);
+    expect(bottom).toHaveBeenCalledTimes(2);
   });
 });
 
 const SwitchedTo = ({ children }) => <div>{children}</div>;
 
-function renderNested() {
+function renderNested({ top = _ => _, bottom = _ => _ }) {
   return render(
     <SwitcherProvider>
-      <ContextReporter />
       <div>
-        <Switcher>
-          <Switcher path="/">
+        <Switcher onChange={top}>
+          <Switcher onChange={bottom} path="/">
             <SwitchedTo path={['/', '/other']}>Home</SwitchedTo>
             <SwitchedTo path="/second">Second</SwitchedTo>
           </Switcher>
